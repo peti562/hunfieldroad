223faa4c7d06fd919392033fbf3cc7a23d7cd760
peti562 <peti562@gmail.com>
2018-02-28 16:15:24 +0200

    write method added

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -22,33 +22,65 @@ function draw(obj){
     context.fill();
 }
 
+function write(obj) {
+    context.font = (obj.font_size+'pt '+obj.font);
+    context.textAlign = 'center';
+    context.fillStyle = obj.color;
+    context.transform(obj.tdata.a, obj.tdata.b, obj.tdata.c, obj.tdata.d, obj.tdata.e, obj.tdata.f);
+    context.fillText(obj.text, obj.pos.x, obj.pos.y);
+};
 
-var block = {       a: { x:0,    y:448 },
-                    b: { x:1000, y:336 },
-                    c: { x:1000, y:700 },
-                    d: { x:0,    y:700 },
-                    color: '#8a1717'
-                    };
 
-var lineabove = {   a: { x:0,    y:441 },
-                    b: { x:1000, y:329 },
-                    c: { x:1000, y:346 },
-                    d: { x:0,    y:458 },
-                    color: '#ffffff'
-                    };
+var block = {
+    a: { x:0,    y:448 },
+    b: { x:1000, y:336 },
+    c: { x:1000, y:700 },
+    d: { x:0,    y:700 },
+    color: '#8a1717'
+};
 
-var miniribbon = {  a: { x:0,   y:463 },
-                    b: { x:285, y:431 },
-                    c: { x:285, y:435 },
-                    d: { x:0,   y:467 },
-                    color: '#ffe400'
-                    };
+var lineabove = {
+    a: { x:0,    y:441 },
+    b: { x:1000, y:329 },
+    c: { x:1000, y:346 },
+    d: { x:0,    y:458 },
+    color: '#ffffff'
+};
 
-var bigribbon = {   a: { x:500,   y:350 },
-                    b: { x:1000,  y:295 },
-                    c: { x:1000,  y:375 },
-                    d: { x:510,     y:430 },
-                    color: '#ffe400'
+var miniribbon = {
+    a: { x:0,   y:463 },
+    b: { x:285, y:431 },
+    c: { x:285, y:435 },
+    d: { x:0,   y:467 },
+    color: '#ffe400'
+};
+
+var bigribbon = {
+    a: { x:500,   y:350 },
+    b: { x:1000,  y:295 },
+    c: { x:1000,  y:375 },
+    d: { x:510,   y:430 },
+    color: '#ffe400'
+};
+
+var ribbontext = {
+    font: 'FFF English Premier League',
+    font_size: '40',
+    color: 'black',
+    transform: true,
+    tdata: { a: 1, b: -0.11, c: 0.1, d: 1, e: 0, f: 0},
+    text: 'VÉGEREDMÉNY',
+    pos: {x: 660, y: 462}
+    };
+
+var social = {
+    font: 'FFF English Premier League',
+    font_size: '40',
+    color: 'black',
+    transform: true,
+    tdata: { a: 1, b: -0.11, c: 0.1, d: 1, e: 0, f: 0},
+    text: 'VÉGEREDMÉNY',
+    pos: {x: 660, y: 462}
 };
 
 
@@ -56,12 +88,10 @@ draw(block);
 draw(lineabove);
 draw(miniribbon);
 draw(bigribbon);
+write(ribbontext);
 
-context.font = '40pt FFF English Premier League';
-context.textAlign = 'center';
-context.fillStyle = 'black';
-context.transform(1, -0.11, 0.1, 1, 0, 0);
-context.fillText('VÉGEREDMÉNY', 660, 462);
+
+
 
 
 console.log(canvas);

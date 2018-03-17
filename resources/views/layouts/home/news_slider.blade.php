344449fb9b867f3cd0cab2ffa75ce70577d263ed
peti562 <peti562@gmail.com>
2018-03-17 10:31:18 +0200

    bugSearch

--- resources/views/layouts/home/news_slider.blade.php
+++ resources/views/layouts/home/news_slider.blade.php
@@ -6,13 +6,13 @@
         <li>
             <!-- News Slider Image -->
             <div>
-                {{--<img src="{{env('APP_URL').'/storage/'.$recentPost->image}}" alt="News Slider 1" style="height:100%">--}}
+                <img src="{{env('APP_URL').'/storage/'.$recentPost->image}}" alt="News Slider 1" style="height:100%">
             </div>
                 <!-- News Slider Content -->
             <div class="news-content">
-                <div class="news-time"><small>{{$recentPost->created_at->diffForHumans()}}</small></div>
-                <div class="news-title big"><h2> <a href="{{env('APP_URL').'posts/'.$recentPost->id}}">{{$recentPost->title}}</a> </h2></div>
-                <p class="news-detail">{{$recentPost->excerpt}} </p>
+                {{--<div class="news-time"><small>{{$recentPost->created_at->diffForHumans()}}</small></div>--}}
+                {{--<div class="news-title big"><h2> <a href="{{env('APP_URL').'posts/'.$recentPost->id}}">{{$recentPost->title}}</a> </h2></div>
+                <p class="news-detail">{{$recentPost->excerpt}} </p>--}}
             </div>
         </li>
         @endforeach
